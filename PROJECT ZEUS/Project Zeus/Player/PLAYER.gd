55cdd54539c64b4a42313b4e2c9a14d49908eca9
SHUBHAM JENA <shubhamjena2020@gmail.com>
2020-11-29 13:19:14 +0530

    Update PLAYER.gd
    ADDED FRICTION ACCELERATION TO THE CHARACTER

--- PROJECT ZEUS/Project Zeus/Player/PLAYER.gd
+++ PROJECT ZEUS/Project Zeus/Player/PLAYER.gd
@@ -1,18 +1,35 @@
 extends KinematicBody2D
+const ACCELERATION = 10
+const MAX_SPEED = 100
+const FRICTION = 10
 
 #basically Vector2 is used to represent position in x and y
 #Vector2.ZERO basically means default value of both x and y is 0
 
 var velocity = Vector2.ZERO
 
+#delta is basically time elasped from the last frame
+#PRO_TIP-if something in our code changes over time and is connected to framerates multiply it by delta
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
-	if input_vector != Vector2.ZERO:
-		velocity = input_vector
-	else:
-		velocity = Vector2.ZERO	
+	#so normally the diagonal distance is more as compared to straight distace.
+	#normalised just makes that distance equal so that our character moves in all direction at the same speed
+	input_vector = input_vector.normalized()
 	
+	
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED*delta)
+	else:
+		#We took the velocity gave it this function move towards
+		#move towards basically adds friction then we gave it two parameters
+		#a vector and another friction which is tied to the framerates by delta
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)	
+		
+#multiplied delta because if the frame drops our velocity will increase
+#by that much factor
+#changed later since already multiplied maxspeed with delta in line 25
 	move_and_collide(velocity)   
